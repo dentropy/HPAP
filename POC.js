fb9e4cbb9817a2d4cc6a80216257e79af9669c8c
dentropy <dentropy@protonmail.com>
2024-11-10 05:42:58 -0500

    Cleaning up more code in the POC

--- POC.js
+++ POC.js
@@ -1,49 +1,43 @@
-// Start with Attestation
-import { SchemaRegistry, EAS, NO_EXPIRATION, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
-import { ethers } from 'ethers'
-// Set EAS Settings
+// Set all the settings
 const easContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e";
 const uid = "0x324897cee471ac2670f8c56d31a691bd542add1f4a88bcfe2494193e397554fa";
 const schemaRegistryContractAddress = "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0";
 const schemaUID = "0x9908e8cf7836eb73b29fb0d6d946cda72ffa75f903d094c64752f44c0e63f7cf";
-// Setup Provider
-let http_url = "https://sepolia.infura.io/v3/063b32ba31b3461ebca9646500a22df3"
-let provider = new ethers.JsonRpcProvider(http_url)
+let provider_http_url = "https://sepolia.infura.io/v3/063b32ba31b3461ebca9646500a22df3"
+let relays = [
+    'wss://relay.damus.io/'
+]
+
+
+
 // Get the Data for the Attestation
+import { SchemaRegistry, EAS, NO_EXPIRATION, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from 'ethers'
+// Setup Provider
+// https://docs.attest.org/docs/developer-tools/eas-sdk#getting-an-attestation
+// The list from the attestation matches to the JSON items in the docs linked in the above line
+let provider = new ethers.JsonRpcProvider(provider_http_url)
 const eas = new EAS(easContractAddress);
 eas.connect(provider);
 const attestation = await eas.getAttestation(uid);
 console.log(Object.keys(attestation));
-for(var item of attestation){
-    console.log("")
-    console.log(item)
-}
+// for(var item of attestation){
+//     console.log("")
+//     console.log(item)
+// }
 // Get the schema from the EAS Registry
 const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
 schemaRegistry.connect(provider);
 const schemaRecord = await schemaRegistry.getSchema({ uid: schemaUID });
-console.log("schemaRecord")
-console.log(schemaRecord)
-console.log(typeof(schemaRecord))
-console.log(Object.keys(schemaRecord))
-console.log(String(schemaRecord))
 let schemaEncoder = new SchemaEncoder(schemaRecord[3])
-console.log("item")
-console.log(attestation)
 let decodeData = schemaEncoder.decodeData(attestation[9])
 console.log(decodeData)
 
 
 
-
 // Get Nostr Event
-
 import { verifyEvent } from 'nostr-tools'
 import { SimplePool } from "nostr-tools/pool";
-import { nip19 } from 'nostr-tools'
-let relays = [
-    'wss://relay.damus.io/'
-]
 export const nostrGet = async (relays, filter) => {
     const pool = new SimplePool();
     const events = await pool.querySync(relays, filter);
@@ -52,13 +46,8 @@ export const nostrGet = async (relays, filter) => {
     console.log(events);
     return events;
 };
-let unix_time = Math.floor((new Date()).getTime() / 1000);
 let nostr_filter = {
     "ids": [decodeData[0].value.value],
-    // since: unix_time - 1000000,
-    // authors: [npub],
-    // kinds: [4],
-    // "#p": npub
   };
 let nostr_events = await nostrGet(relays, nostr_filter)
 let the_nostr_event = nostr_events[0]
@@ -66,8 +55,7 @@ if( !verifyEvent(the_nostr_event)){
     console.log("Nostr Event Did Not Check Out")
     process.exit()
 }
-console.log(the_nostr_event)
-// Get the CID
+// Get the CID, IID(Internet Identifier), and AI Generated Fact
 let nostr_event_data = {
     CID : "",
     IID : "",
@@ -93,19 +81,10 @@ for(let element of the_nostr_event.tags){
 console.log(nostr_event_data)
 
 
+
 // Validate NIP05
-
-// For Testing
-// let nostr_event_data = {}
-// nostr_event_data.IID = "paul@ddaemon.org"
-console.log("nostr_event_data")
-console.log(nostr_event_data)
-
-
-import {  useFetchImplementation, queryProfile, NIP05_REGEX, isNip05 } from 'nostr-tools/nip05'
 let username = nostr_event_data.IID[1].split("@")[0]
 let domain_name = nostr_event_data.IID[1].split("@")[1]
-let profile = await queryProfile(String(domain_name))
 let response = await fetch(`https://${domain_name}/.well-known/nostr.json?name=${username}`)
 response = await response.json()
 if(response.names[username] != the_nostr_event.pubkey){
@@ -115,10 +94,12 @@ if(response.names[username] != the_nostr_event.pubkey){
     console.log("NIP05 Verification SUCCESS")
 }
 
+
+
 // Get the actual content and validate it
 import { sha256 } from '@noble/hashes/sha2';
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils'
-let content = await fetch("https://blossom.newatlantis.top/eca61dae93d2bde2c7eefd764cdbc8c31650a41d5da742b67e6efcd8931ae596")
+let content = await fetch(nostr_event_data.CID[2])
 let content_buffer = await content.arrayBuffer()
 let content_hex_hash = bytesToHex(sha256(new Uint8Array(content_buffer)))
 if(nostr_event_data.CID[1].toLowerCase() != content_hex_hash.toLowerCase()){
@@ -127,6 +108,8 @@ if(nostr_event_data.CID[1].toLowerCase() != content_hex_hash.toLowerCase()){
 } else {
     console.log("Content hash matches")
 }
+
+
 
 // Get Cubid Cubid Score
 let cubid_user_response = await fetch("https://passport.cubid.me/api/v2/create_user", {
@@ -152,3 +135,4 @@ let cubid_user_score_response = await fetch("https://passport.cubid.me/api/v2/sc
 });
 cubid_user_score_response = await cubid_user_score_response.json()
 console.log(cubid_user_score_response)
+process.exit()
